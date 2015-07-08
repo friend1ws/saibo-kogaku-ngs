5f81c0497bc4c7783dc296858045925ecc4640aa
Yuichi Shiraishi <friend1ws@gmail.com>
2015-07-08 17:02:42 +0900

    modify runall.sh

--- script/runall.sh
+++ script/runall.sh
@@ -13,15 +13,15 @@ while read sample;
 do 
     # filter splicing junctinos with >= 5 support reads
     echo "python simpleFilt.py ../data/mapsplice/${sample}C.junctions.txt.gz 5 > ../result/${sample}.junctions.filt.txt"
-    # python simpleFilt.py ../data/mapsplice/${sample}C.junctions.txt.gz 5 > ../result/${sample}.junctions.filt.txt
+    python simpleFilt.py ../data/mapsplice/${sample}C.junctions.txt.gz 5 > ../result/${sample}.junctions.filt.txt
 
     # annotate splicing junctions (exon skip, splicing-site slip, pseudo exon inclusion and so on) 
     echo "python annotSplicing.py ../result/${sample}.junctions.filt.txt ../data/db/refGene.bed.gz ../data/db/refExon.bed.gz > ../result/${sample}.annot.txt"
-    # python annotSplicing.py ../result/${sample}.junctions.filt.txt ../data/db/refGene.bed.gz ../data/db/refExon.bed.gz > ../result/${sample}.annot.txt
+    python annotSplicing.py ../result/${sample}.junctions.filt.txt ../data/db/refGene.bed.gz ../data/db/refExon.bed.gz > ../result/${sample}.annot.txt
 
     # extract putative mutations causing splicing changes
     echo "python getSplicingMut.py ../result/${sample}.annot.txt ../data/vcf/${sample}.mutation.vcf.gz ../data/db/refExon.bed.gz > ../result/${sample}.mut2sp.txt"
-    # python getSplicingMut.py ../result/${sample}.annot.txt ../data/vcf/${sample}.mutation.vcf.gz ../data/db/refExon.bed.gz > ../result/${sample}.mut2sp.txt
+    python getSplicingMut.py ../result/${sample}.annot.txt ../data/vcf/${sample}.mutation.vcf.gz ../data/db/refExon.bed.gz > ../result/${sample}.mut2sp.txt
 
     echo "python mergeMut.py ../data/vcf/${sample}.mutation.vcf.gz ../result/${sample}.mut2sp.txt ../data/db/refExon.bed.gz ${sample} >> ../result/RK.mut2trans.txt"
     python mergeMut.py ../data/vcf/${sample}.mutation.vcf.gz ../result/${sample}.mut2sp.txt ../data/db/refExon.bed.gz ${sample} >> ../result/RK.mut2trans.txt
